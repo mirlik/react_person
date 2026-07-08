bcc5aafbf32cb10a04c9382bff0475c352fa44c1
Alex <11165950+mirlik@users.noreply.github.com>
2026-07-08 11:26:02 +0300

    fix#1

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -4,11 +4,12 @@ export const Person = ({ person }) => {
     ? `${person.partnerName} is my ${partnerSex}`
     : 'I am not married';
   const ageText = person.age ? `I am ${person.age}` : '';
+  const shouldShowAge = Boolean(ageText);
 
   return (
     <div className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
-      {ageText && <p className="Person__age">{ageText}</p>}
+      {shouldShowAge && <p className="Person__age">{ageText}</p>}
       <p className="Person__partner">{partnerText}</p>
     </div>
   );
